87e1fa38faaefce56ac8e94d5b79d122cd655686
IbragimAzimov <ibragimik76@gmai.coml>
2022-05-30 23:56:53 +0600

    Произведен расчет формулы для нахождения площади круга.

--- src/com/company/Circle.java
+++ src/com/company/Circle.java
@@ -2,7 +2,19 @@ package com.company;
 
 public class Circle {
     private int radius;
+    private double y;
 
+    public int getRadius() {
+        return radius;
+    }
 
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
 
+    public double getY() {
+        double PI = 3.14;
+        y = PI * radius * radius;
+        return y;
+    }
 }
